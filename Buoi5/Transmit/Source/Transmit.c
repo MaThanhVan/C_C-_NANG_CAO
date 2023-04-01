a5e6df9bf65e71a1d36ce456472d01ef77b85200
MaThanhVan <119687191+MaThanhVan@users.noreply.github.com>
2023-04-01 22:03:48 +0700

    Update Transmit.c

--- Buoi5/Transmit/Source/Transmit.c
+++ Buoi5/Transmit/Source/Transmit.c
@@ -7,19 +7,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include "../Header/Transmit.h/"
 
-typedef union 
-{
-    struct{
-        char uid[5];        //mang uid de luu ID vao 
-        char data[9];       //mang data d luu data vao
-    }frame;                     //member 1
-    char data_frame[12];      
-}frame_nfc;                       //member 2
-
-frame_nfc nfc;
-
-/*
 * Function: Transmit 
 * Description: This function 
 * Input:
@@ -37,8 +26,3 @@ char* Transmit(char* ID, char* DATA)
     return strcat(nfc.frame.uid, nfc.frame.data);  ///strcat dung de noi hai chuoi
 }
 
-int main(int ardc, char const *argv[])
-{
-    printf("%s",  Transmit((char*)"1001", (char*)"10100110" ));
-    return 0;
-}
