961a0831f6dc02b5e210b5d32e1b3a6cb24f7acb
MaThanhVan <119687191+MaThanhVan@users.noreply.github.com>
2023-04-01 17:52:47 +0700

    Update Check_Cart.c

--- Buoi7_BitMask/Source/Check_Cart.c
+++ Buoi7_BitMask/Source/Check_Cart.c
@@ -9,35 +9,44 @@
 #include "../Header/Check_Cart.h"
 
 
-/**
- * @brief Function to add the clothes in to cart
- *
- * @param gio_hang Pointer used to add widgets in to cart
- * @param loai_do_dung widgets add
- */
+/*
+* Function: Them_vao_gio_hang 
+* Description: Function to add the clothes in to cart
+* Input:
+*    gio_hang Pointer used to add widgets in to cart
+*    loai_do_dung widgets add
+* Output:
+*   
+*/
 void Them_vao_gio_hang(uint8_t *gio_hang, DO_DUNG_CA_NHAN loai_do_dung)
 {
     *gio_hang |= loai_do_dung;      // phep or bit a |= b tuong duong a = a|b
 }
 
 
-/**
- * @brief Function to clear the clothes in to cart
- *
- * @param gio_hang Pointer management cart
- * @param loai_do_dung widgets  you want clear
- */
+/*
+* Function: Xoa_khoi_gio_hang 
+* Description: Function to clear the clothes in to cart
+* Input:
+*    gio_hang Pointer management cart
+*    loai_do_dung widgets  you want clear
+* Output:
+*   
+*/
 void Xoa_khoi_gio_hang(uint8_t *gio_hang, DO_DUNG_CA_NHAN loai_do_dung)
 {
     *gio_hang &= ~loai_do_dung;     // phep and bit
 }
 
-/**
- * @brief Function to check the clothes in to cart
- *
- * @param gio_hang Pointer used to add widgets 
- * @param loai_do_dung widgets you want check
- */
+/*
+* Function: Kiem_tra_hang 
+* Description: Function to check the clothes in to cart
+* Input:
+*    gio_hang Pointer used to add widgets
+*    loai_do_dung widgets you want check
+* Output:
+*   
+*/
 void Kiem_tra_hang(uint8_t *gio_hang, DO_DUNG_CA_NHAN do_dung)
 {
     DO_DUNG_CA_NHAN temp = do_dung;
@@ -51,11 +60,15 @@ void Kiem_tra_hang(uint8_t *gio_hang, DO_DUNG_CA_NHAN do_dung)
     Kiem_tra_san_pham(TUI);
 }
 
-/**
- * @brief Function to display the clothes in to cart
- *
- * @param gio_hang Pointer used to add widgets 
- */
+/*
+* Function: Hien_thi_gio_hang 
+* Description: Function to display the clothes in to cart
+* Input:
+*    gio_hang Pointer used to add widgets
+*    
+* Output:
+*   
+*/
 void Hien_thi_gio_hang(uint8_t *gio_hang)
 {
     int i=0;
@@ -70,9 +83,3 @@ void Hien_thi_gio_hang(uint8_t *gio_hang)
     KIEM_TRA_GIO_HANG(TUI);
 }
 
-int main(int argc, char const *argv[])
-{
-    Them_vao_gio_hang(&gio_hang, NHAN|AO|GIAY|QUAN);
-    Hien_thi_gio_hang(&gio_hang);
-    Kiem_tra_hang(&gio_hang, NHAN|AO|GIAY|QUAN|VAY|DAM|VONG_TAY|TUI);
-}
