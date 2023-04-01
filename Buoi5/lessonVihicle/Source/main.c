d9426e08e2d73d103ee9c68de09b9d00ca07d544
MaThanhVan <119687191+MaThanhVan@users.noreply.github.com>
2023-04-01 17:58:16 +0700

    Create main.c

---
+++ Buoi5/lessonVihicle/Source/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "../Header/LessonVehicle.h"
+
+
+int main(int argc, char const *argv[])
+{
+    phuong_tien_giao_thong phuong_tien;
+
+    chuong_trinh_chon_duong(&phuong_tien);
+
+    
+
+    return 0;
+}
