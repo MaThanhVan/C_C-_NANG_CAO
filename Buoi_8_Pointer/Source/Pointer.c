5eb5437c7a89c504ccadbb3b8299bcddb6c7d3ac
MaThanhVan <119687191+MaThanhVan@users.noreply.github.com>
2023-04-01 21:48:37 +0700

    Create Pointer.c

---
+++ Buoi_8_Pointer/Source/Pointer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "../Header/Pointer.h"
+
+* Function: calculate_Sum
+* Description: This function calculates the sum of two integers
+* Input:
+*   a - an integer value
+*   b - an integer value
+* Output:
+*   
+*/  
+void calculate_Sum(int a, int b){
+    printf("tong %d va %d = %d\n", a, b, a+b);
+}
+
+* Function: calculate_Subtraction
+* Description: This function calculates the suctraction of two integers
+* Input:
+*   a - an integer value
+*   b - an integer value
+* Output:
+*   
+*/
+void calculate_Subtraction(int a, int b){
+    printf("hieu %d va %d = %d\n", a, b, a-b);
+}
+
