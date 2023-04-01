69f4c167c27f9a8dbf3e6700dfd67d55b5c5c96f
MaThanhVan <119687191+MaThanhVan@users.noreply.github.com>
2023-04-01 22:04:23 +0700

    Update Transmit.h

--- Buoi5/Transmit/Header/Transmit.h
+++ Buoi5/Transmit/Header/Transmit.h
@@ -1,1 +1,19 @@
+#ifndef __TRANSMIT_H
+#define __TRANSMIT_H
+
+#include <stdio.h>
+
+typedef union 
+{
+    struct{
+        char uid[5];        //mang uid de luu ID vao 
+        char data[9];       //mang data d luu data vao
+    }frame;                     //member 1
+    char data_frame[12];      
+}frame_nfc;                       //member 2
+
+frame_nfc nfc;
+char* Transmit(char* ID, char* DATA)
+
+#endif
 
