f3cf2c8cd5c24316b206d81276f016a09e334893
MaThanhVan <119687191+MaThanhVan@users.noreply.github.com>
2023-04-01 19:32:19 +0700

    Update main.c

--- Buoi5/Transmit/Source/main.c
+++ Buoi5/Transmit/Source/main.c
@@ -1,14 +1,6 @@
 #include "../Header/Transmit.h"
 #include <stdio.h>
 
-char* Transmit(char* ID, char* DATA)
-{
-    
-    strcpy(nfc.frame.uid, ID);         // Truyền data vào uid
-    strcpy(nfc.frame.data, DATA);       // Truyền data vào data 
-    
-    return strcat(nfc.frame.uid, nfc.frame.data);  ///strcat dung de noi hai chuoi
-}
 
 int main(int ardc, char const *argv[])
 {
